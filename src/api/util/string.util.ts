e293d12ece67a0955db131c94b428303d8951726
Ahmed Musallam <ahmed.musallam@kp.org>
2017-05-28 21:51:56 -0500

    remove unused

--- src/api/util/string.util.ts
+++
@@ -1,8 +0,0 @@
-export class StringUtil
-{
-  
-  private static getInitials(str:string){
-    var initials = str.match(/\b\w/g) || [];
-    return ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
-  }
-}
